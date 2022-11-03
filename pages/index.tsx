4b5d7126cead150b3049c8ad2689db222feb88e3
Pathawee <pathawee.somsak@gmail.com>
2022-11-03 04:33:53 +0100

    fix menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <Menu/>
       <div className="container col-xxl-8 px-4 py-5">
         <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-          <div className="col-10 col-sm-8 col-lg-6">
+          <div className="col-10 col-sm-8 col-lg-6 text-center">
             <img src="/pae-image.png" alt="" width="auto" height="500" className="bi me-2" />
           </div>
           <div className="col-lg-6">

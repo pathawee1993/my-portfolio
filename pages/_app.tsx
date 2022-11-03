6456ff921cf5cc266bc0773576ec26e489dcdabd
Pathawee <pathawee.somsak@gmail.com>
2022-11-03 04:00:34 +0100

    fix menu

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { useEffect } from 'react'
+import Script from 'next/script'
+import { useEffect, useState } from 'react'
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    import ('bootstrap/dist/js/bootstrap.bundle.min.js')
-  }, [])
-  return <Component {...pageProps} />
+  // useEffect(() => {
+  //   import ('bootstrap/dist/js/bootstrap.bundle.min.js')
+  // }, [])
+  // return <Component {...pageProps} />
+  return (
+    <>
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.2/js/bootstrap.min.js" />
+      <Component {...pageProps} />
+    </>
+  )
 }
